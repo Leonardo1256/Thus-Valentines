148c96a2861b7db0175361e6306d13bccf9be812
Leonardo1256 <narutofacts.nguyen1@gmail.com>
2025-02-02 13:19:26 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,8 @@
 
 function selectOption(option) {
     if (option === 'yes') {
-        // Flash rainbow colors
-        flashRainbowColors(function() {
-            document.getElementById('question').style.display = 'none';
-            displayPonyo();
-        });
+        document.getElementById('question').style.display = 'none';
+        displayPonyo();
     } else if (option === 'no') {
         // Change text on the "No" button to "You sure?"
         document.getElementById('no-button').innerText = 'You sure?'; 
@@ -19,24 +16,6 @@ function selectOption(option) {
         alert('Invalid option!');
     }
 }
-
-// Function to flash rainbow colors and then execute a callback function
-function flashRainbowColors(callback) {
-    var colors = ['#ff0000', '#ff7f00', '#ffff00', '#00ff00', '#0000ff', '#4b0082', '#9400d3'];
-    var i = 0;
-    var interval = setInterval(function() {
-        document.body.style.backgroundColor = colors[i];
-        i = (i + 1) % colors.length;
-    }, 200); // Change color every 200 milliseconds
-    setTimeout(function() {
-        clearInterval(interval);
-        document.body.style.backgroundColor = ''; // Reset background color
-        if (callback) {
-            callback();
-        }
-    }, 2000); // Flash colors for 2 seconds
-}
-
 
 function displayStand() {
     var imageContainer = document.getElementById('image-container');
